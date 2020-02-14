4475ed130d95ddfcccf1ae3b06fddbae4de50410
LukindoSandra <sandramaryanne9@gmail.com>
2020-02-14 17:35:16 +0300

    added A script.js file

---
+++ script.js
@@ -0,0 +1,8 @@
+var day=document.getElementById('day').innerHTML
+var day=document.getElementById('month').innerHTML
+var day=document.getElementById('year').innerHTML
+
+var maleNames=['Kwasi','Kwado','Kwabena','Kwaku','Yaw','Kofi','Kwame']
+var femaleNames=['Akosua','Adwoa','Abenna','Akua','Yaa','Afua','Ama']
+
+
