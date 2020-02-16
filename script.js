699118e8462ffab6bf32e22d3b1ba2502ea4f782
LukindoSandra <sandramaryanne9@gmail.com>
2020-02-16 18:02:20 +0300

    update on the functions

--- script.js
+++ script.js
@@ -26,4 +26,31 @@ else if(month<=0|| month > ){
     alert("Invalid month should be between 1 and 12");
 }
 
+else if(day<=0 || day>31){
+    alert("invalid date");
+}
+
+else if(validate==false){
+    alert("invalid input");
+}
+
+if(gender==="male"&&year > 0 && month > 0 && month <13 && day <32){
+    akanName=maleNames[dayBorn];
+    alert("You were born on" + dayOfTheWeek[dayBorn]+ "and your Akan name is" +akanName );
+}
+
+else if(gender==="female"&&year> 0&& month> 0&&month <13&&day> 0&&day<32){
+    akanName=femaleNames[dayBorn];
+    alert("You were born on" + dayOfTheWeek[dayBorn] +"and your Akan name is" +akanName );
+}
+
+functiongetGender(){
+    var gender= document.getElementsByName("gender");
+    for( i= 0;i<gender.length;i++){
+        if(gender[i].checked){
+            return(gender[i].value)
+        }
+    }
+}
+
 
