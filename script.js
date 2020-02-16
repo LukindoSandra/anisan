ac66cef4b00fcbf23b7b11d52db4f699f77519fe
LukindoSandra <sandramaryanne9@gmail.com>
2020-02-16 16:41:30 +0300

    addition of functions

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ var day=document.getElementById('day').innerHTML
 var day=document.getElementById('month').innerHTML
 var day=document.getElementById('year').innerHTML
 
-var maleNames=['Kwasi','Kwado','Kwabena','Kwaku','Yaw','Kofi','Kwame']
-var femaleNames=['Akosua','Adwoa','Abenna','Akua','Yaa','Afua','Ama']
+var maleNames=['Kwasi','Kwado','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
+var femaleNames=['Akosua','Adwoa','Abenna','Akua','Yaa','Afua','Ama'];
+
+function akanName(){
+    var day=parseInt(document.getElementById('day')).value;
+    var month=parseInt(document.getElementById('month')).value;
+    var year=parseInt(document.getElementById('year')).value;
+    
+};
+
+var gender=getGender();
+var date=new Date
 
 
