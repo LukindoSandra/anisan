b05cbc8d3f6397202c7324051a2782788fa0d607
LukindoSandra <sandramaryanne9@gmail.com>
2020-02-18 12:52:21 +0300

    committed to the gh-pages

--- script.js
+++ script.js
@@ -1,56 +1,44 @@
-var day=document.getElementById('day').innerHTML
-var day=document.getElementById('month').innerHTML
-var day=document.getElementById('year').innerHTML
 
-var maleNames=['Kwasi','Kwado','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
-var femaleNames=['Akosua','Adwoa','Abenna','Akua','Yaa','Afua','Ama'];
-
-function akanName(){
-    var day=parseInt(document.getElementById('day')).value;
-    var month=parseInt(document.getElementById('month')).value;
-    var year=parseInt(document.getElementById('year')).value;
-    
-};
-
-var gender=getGender();
-var date=new Date(year+"/" +month+"/"+day);
-var dayBorn=date.getDay();
-var akanNameName;
-var validateGender=(gender!=="male"&&gender!=="female");
-
-if(year<=0){
-    alert("The is invalid");
+var maleNames = ["Kwasi", "Kwandwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+var dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday"];
+function giveAkanName () {
+    var year =parseInt(document.getElementById("year").value);
+    var month=parseInt(document.getElementById("month").value);
+    var day=parseInt(document.getElementById("day").value);
+    var gender=getGender();
+    var date = new Date(year +"/" + month + "/"+day);
+    var dayBorn = date.getDay();
+    var akanName;
+    var validate = (year > 0 && (month >0 && month <=12) && (day > 0 && day <= 31));
+    var validateGender = (gender !== "male" && gender !== "female");
+    if (year <= 0 ) {
+        alert("The is invalid");
+    }
+    else if (month <= 0 || month > 12) {
+        alert("Invalid month should be between 1 and 12");
+    }
+    else if (day <= 0 || day > 31){
+        alert("Invalid date");
+    }
+    else if (validate == false) {
+        alert("Invalid Input");
+    }
+    if(gender ==="male" && year > 0 && month > 0 && month < 13 && day > 0 && day < 32){
+        akanName = maleNames[dayBorn];
+    alert("You were born on "+ dayOfTheWeek[dayBorn]+ " and your Akan name is " +akanName);
+    }
+    else if (gender === "female" && year > 0 && month > 0 && month < 13 && day > 0 && day < 32){
+        akanName = femaleNames[dayBorn];
+    alert("You were born on "+ dayOfTheWeek[dayBorn]+ " and your Akan name is " +akanName);
+    }
 }
-
-else if(month<=0|| month >12) {
-    alert("Invalid month should be between 1 and 12");
-}
-
-else if(day<=0 || day>31){
-    alert("invalid date");
-}
-
-else if(validate==false){
-    alert("invalid input");
-}
-
-if(gender==="male"&&year > 0 && month > 0 && month <13 && day <32) {
-    akanName=maleNames[dayBorn];
-    alert("You were born on" + dayOfTheWeek[dayBorn]+ "and your Akan name is" +akanName );
-}
-
-else if(gender==="female"&&year> 0&& month> 0&&month <13&&day> 0&&day<32) {
-    akanName=femaleNames[dayBorn];
-    alert("You were born on" + dayOfTheWeek[dayBorn] +"and your Akan name is" +akanName );
-}
-
-functiongetGender(){
-    var gender= document.getElementsByName("gender");
-    for( i= 0;i<gender.length;i++) {
-        if(gender[i].checked) {
-            return(gender[i].value)
+    function getGender(){
+        var gender = document.getElementsByName("gender");
+        for(i = 0; i < gender.length; i++){
+            if(gender[i].checked){
+                return(gender[i].value)
         }
     }
 }
-
 
